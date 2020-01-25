52cea9c19290123c211d2d5bb145b6370c473d5f
Simardeep Singh <42577748+tombstoneghost@users.noreply.github.com>
2020-01-25 12:51:18 +0530

    Create Day_0.java

---
+++ 30 Days of Code/Day_0.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Day0
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        String str;
+        str = sc.nextLine();
+
+        System.out.println("Hello, World.");
+        System.out.println(str);
+    }
+}
